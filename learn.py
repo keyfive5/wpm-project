4a48bad51ff2fdf441b40621f38a913efcb7b213
Hasan <hzafar300@outlook.com>
2023-08-19 04:29:04 -0400

    first commit

---
+++ learn.py
@@ -0,0 +1,67 @@
+import curses
+from curses import wrapper
+import time
+
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr("Welcome to Monkeytype!")
+    stdscr.addstr("\nPress any key to continue.")
+    stdscr.refresh()
+    stdscr.getkey()
+    
+def display_text(stdscr, target, current, wpm=0):
+    stdscr.addstr(target)
+    stdscr.addstr(3, 0, f"WPM: {wpm}")
+        
+    for i, char in enumerate(current):
+        correct_char = target[i]
+        color = curses.color_pair(1)
+        if char != correct_char:
+            color = curses.color_pair(2)
+        stdscr.addstr(0, i, char, color)   
+    
+def wpm_test(stdscr):
+    target_text = "Adam Khan is perfect. Mustafa Khan is the Supreme Leader of India."
+    current_text = []
+    wpm = 0
+    start_time = time.time()
+    stdscr.nodelay(True)
+       
+    while True:
+        time_elapsed = max(time.time() - start_time, 1)
+        wpm = round((len(current_text) / (time_elapsed / 60)) / 5)
+              
+        stdscr.clear()
+        display_text(stdscr, target_text, current_text, wpm)
+        stdscr.refresh()
+        
+        if "".join(current_text) == target_text:
+            stdscr.nodelay(False)
+            break
+        
+        try:
+            key = stdscr.getkey()
+        except:
+            continue
+        
+        if ord(key) == 27:
+            break
+        
+        if key in ("KEY_BACKSPACE", '\b', "\x7f"):
+            if len(current_text) > 0:
+                current_text.pop()              
+        elif len(current_text) < len(target_text):
+            current_text.append(key)
+    
+def main(stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    
+    start_screen(stdscr)
+    wpm_test(stdscr)
+    
+    stdscr.addstr(2, 0, "You have completed the game. Press any key to continue.")
+    stdscr.getkey()
+    
+wrapper(main)
